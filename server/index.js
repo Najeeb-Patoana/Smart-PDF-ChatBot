bcc2de61ff1b0b51cfb675c8a05ba16ed9ce29c5
Najeeb Patoana <najeebpatoana007@gmail.com>
2026-07-03 15:46:30 +0500

    working

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
-const express = require('express')
+const express = require('express');
+const multer = require('multer');
 
 const app = express();
 
 
-
+const upload = multer({dest:"uploads"})
 
 app.get('/',(req,res)=>{
     
      res.send("Najeeb Ullah Khan")
 })
 
+app.post('/upload',upload.single("pdf"),(req,res)=>{
+    console.log(res.file);
+    res.send("file uploaded done!")
+    
+
+}) 
 
 app.listen(3000,()=>{
 
